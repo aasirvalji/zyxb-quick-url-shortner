d2191016b8df2cc918d5993b92a356f9c8a00fe1
av <45238682+avxlji@users.noreply.github.com>
2020-08-01 00:45:55 -0400

    added v1/url/shorten route

--- routes/v1/url.js
+++ routes/v1/url.js
@@ -2,12 +2,43 @@ const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
 const path = require('path');
+const validUrl = require('valid-url');
+const shortid = require('shortid');
+const Url = require('../../models/Url');
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 router.post('/shorten', async (req, res) => {
   var longUrl = req.body.url;
   var baseUrl = process.env.devUrl;
+
+  if (!validUrl.isUri(baseUrl))
+    return res.status(400).json({ message: 'Invalid base url' });
+
+  var shortCode = shortid.generate();
+
+  if (!validUrl.isUri(longUrl))
+    return res.status(400).json({ message: 'Invalid long url' });
+
+  try {
+    var url = await Url.findOne({ longUrl });
+
+    if (url) res.json(url);
+    else {
+      var newShortUrl = new Url({
+        shortUrl: process.env.devUrl + '/' + shortCode,
+        longUrl,
+        shortCode,
+      });
+
+      await newShortUrl.save();
+
+      res.json(newShortUrl);
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
