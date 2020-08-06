ea5cb62d804021a95296ac85832d6aa0afd261c3
av <45238682+avxlji@users.noreply.github.com>
2020-08-06 11:48:33 -0400

    updated styling

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import Heading from './components/Heading/Heading';
 //material ui imports
 import Button from '@material-ui/core/Button';
 import CropFreeIcon from '@material-ui/icons/CropFree';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import Divider from '@material-ui/core/Divider';
 
 function App() {
   const [urlInput, setUrlInput] = useState(null);
@@ -24,14 +27,15 @@ function App() {
         )
       ) {
         axios.post('/api/url/shorten', { url: urlInput }).then((res) => {
-          if (shortUrls.length > 2) {
-            var longUrl = shortUrls.slice(0,3)
-            setShortUrls(longUrls)
+          if (shortUrls.length > 4) {
+            let su = [...shortUrls]
+            su = su.slice(0, 4)
+            setShortUrls(su)
           }
-          if (longUrls.length > 2) 
+          if (longUrls.length > 4) 
           {
             let lu = [...longUrls]
-            lu = lu.slice(0, 2)
+            lu = lu.slice(0, 4)
             setLongUrls(lu)
           }
           console.log(shortUrls, longUrls)
@@ -51,6 +55,31 @@ function App() {
   const clearFields = () => {
     setUrlInput('');
   };
+
+  function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData("Text", text);
+
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
 
   return (
     <Router>
@@ -79,36 +108,75 @@ function App() {
               variant="contained"
               color="secondary"
               onClick={() => clearFields()} 
-              startIcon={<CropFreeIcon />}
+              startIcon={<HighlightOffIcon />}
               id="clear-fields">
               Clear Entry
             </Button>
           </div>
-
-          <div className="short-url-container">
+      
+          <div className="result-container">
+          <h1>Shortened Url</h1>
             {!(shortUrls === undefined || shortUrls.length == 0) && 
             <>
-            <div>
-         
+            <div className='result-content'>
+            <div className='first-short-url-and-copy'>
             <p>
-              Short Url: {shortUrls[0]}
-              shorturl length: {shortUrls.length}
+              Short Url: <b>{shortUrls[0]}</b>
+              {/* shorturl length: {shortUrls.length} */}
             </p>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<AssignmentIcon />}
+              onClick={() => copyToClipboard(shortUrls[0])}
+              id="clipboard">
+              Copy
+            </Button>
+            </div>
+            <div className='long-short-url'>
             <p>
-              Long Url: {longUrls[0]}
-              longurl length: {longUrls.length}
+              Long Url: <b>{longUrls[0]}</b>
+              {/* longurl length: {longUrls.length} */}
             </p>
             </div>
+            {(shortUrls.length > 1 && longUrls.length > 1) && <Divider style={{backgroundColor: 'white'}} className='divider'/>}
+            </div>
 
+            <h1>Recent Url's</h1>
             <div className='recent-urls-container'>
             {!(shortUrls === undefined || shortUrls.length < 2) && 
-            <>
-            {shortUrls.map(shortUrl => (
+            <div className='recent-content'>
+            {shortUrls.map((shortUrl, index) => (
               <>
-              {shortUrl}
+              {index > 0 && 
+                <div className='result-content'>
+            <div className='first-short-url-and-copy'>
+            <p>
+              Short Url: <b>{shortUrl}</b>
+              {/* shorturl length: {shortUrls.length} */}
+            </p>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<AssignmentIcon />}
+              id="clipboard"
+              onClick={() => copyToClipboard(shortUrl)}
+              >
+              Copy
+            </Button>
+            </div>
+            <div className='long-short-url' id={index === shortUrls.length - 1 ? 'last-recent-url' : ''}>
+            <p>
+              Long Url: <b>{longUrls[index]}</b>
+              {/* longurl length: {longUrls.length} */}
+            </p>
+            </div>
+            {(index < shortUrls.length - 1) && <Divider style={{backgroundColor: 'white'}} className='divider'/>}
+            </div>
+              }
               </>
             ))}
-            </>
+            </div>
             }
             </div>
             </>
