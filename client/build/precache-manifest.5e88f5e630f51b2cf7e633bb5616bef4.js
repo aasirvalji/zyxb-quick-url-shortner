d711c1ac17b6654374b891e54160cf1a46eb6a7c
av <45238682+avxlji@users.noreply.github.com>
2020-08-16 10:18:28 -0400

    updated gitignore and added client build

---
+++ client/build/precache-manifest.5e88f5e630f51b2cf7e633bb5616bef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135c3e460ff01ae7c370da46d9df80ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909b07d2f56727816298",
+    "url": "/static/css/main.83fce501.chunk.css"
+  },
+  {
+    "revision": "0cbd417136c66b7fd17a",
+    "url": "/static/js/2.e45e513a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e45e513a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909b07d2f56727816298",
+    "url": "/static/js/main.2e4a9dd4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "adc085fb2df5606caccbd7c019c1bd32",
+    "url": "/static/media/test.adc085fb.png"
+  }
+]);
